c5641263175bacddca75fe3ed3ea31aafa810ac8
Sidd <sidd.bhattacherya3@gmail.com>
2020-01-12 09:30:35 -0800

    On branch master

--- Content.js
+++ Content.js
@@ -1,13 +1,3 @@
-/*function getSelectionText() {
-    var text = "";
-    if (window.getSelection) {
-        text = window.getSelection().toString();
-    } else if (document.selection && document.selection.type != "Control") {
-         text = document.selection.createRange().text;
-    }
-    return text;
-}
-*/
 
 $(window).on('load', function(){
     window.setTimeout(getMessages, 5000)});
@@ -27,19 +17,7 @@ function getMessages(){
                 }
                 
             })
-            let length = messages.length
-            /*for (var i = 0; i<length;i++) {
-                JSONMessages[i] = JSON.stringify(messages[i]);
-            }
-            //sendToPython(JSONMessages, div); */
-}
-
-function sendToPython(messages, div)
-{
-    for(var i = 0; i<messages.length;i++) {
-        sendSingular(messages[i], div)
-        ;
-    }
+                      
 }
 
 function sendSingular(text, div){
@@ -56,27 +34,10 @@ function sendSingular(text, div){
     }
 }
     xhttp.open("GET", url, true)
-    // xhttp.withCredentials = true;
-    // xhttp.setRequestHeader("Content-Type", "application/json");
-    // xhttp.send({ 'request': "authentication token" });
     xhttp.send()
 
 }
-/*
-function getEmojiAndPost(sentence){
-    var emojiCODE ="";
-    if (sentence == "Happy"){emojiCODE = "\uD83D\uDE00"}
-    else if (sentence == "Angry"){emojiCODE = "\uD83D\uDE20" }
-    else if (sentence == "Excited"){ emojiCODE = "\uD83E\uDD29" }
-    else if (sentence == "Sad"){emojiCODE = "\uD83D\uDE29" }
-    else if (sentence == "Fear"){emojiCODE = "\uD83D\uDE31" }
-    else if (sentence == "Bored"){ emojiCODE = "\uD83D\uDE34"}
-    else {emojiCODE = "no associated emotion"}
-    return emojiCODE;
 
-    
-}
-*/
 function getEmojiAndPost(sentence){
     //console.log(sentence);
     var emojiCODE ="";
@@ -90,18 +51,6 @@ function getEmojiAndPost(sentence){
     else {emojiCODE = "\uD83D\uDE10"}
     return emojiCODE;
 }
-
-
-
-  /*window.addEventListener('mouseup', e => {
-    getMessages();
-    //setEmojis(messages);
-
-
-});*/
-
-//$("body").on('DOMSubtreeModified', getMessages())
-
 
 
 
