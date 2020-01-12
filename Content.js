fe7597ffffdc0b8795a000102c2d766bbba3a320
asamra <anandsamra@hotmail.com>
2020-01-11 22:14:08 -0800

    [Added the emoji sentance translation]

--- Content.js
+++ Content.js
@@ -10,13 +10,26 @@
 */
 
 function getMessages(){
-    var messages = [];
-    let divs = document.getElementsByClassName("uiScrollableAreaContent");
-    console.log(div.innerText);
+    document.getElementsByClassName('uiScrollableAreaContent')[2].querySelector('div').querySelectorAll('.direction_ltr, .text_align_ltr')
+        .forEach(function(div) {
+                let query = div.querySelector('div[tabindex] > span');
+                if (query) {
+                    console.log(query.innerText)
+                }
+            }
+        )
 
 }
 function getEmoji(sentence){
-
+    var emojiCODE ="";
+    if (sentence == "HAPPY"){emojiCODE = "U+1F600"}
+    else if (sentence == "ANGRY"){emojiCODE = "U+1F620" }
+    else if (sentence == "EXCITED"){ emojiCODE = "U+1F929"}
+    else if (sentence == "SAD"){emojiCODE = "U+1F629" }
+    else if (sentence == "FEAR"){emojiCODE = "U+1F631" }
+    else if (sentence == "BORED"){ emojiCODE = "U+1F634"}
+    else {emojiCODE = "no associated emotion"}
+    return emojiCODE;
 }
 
 
@@ -24,18 +37,12 @@ function getEmoji(sentence){
 window.addEventListener('mouseup', e => {
     getMessages();
     setEmojis(messages);
-    //getSelectionText();
+
 
 });
 
 
-function setEmojis(messageLocations){
-    foreach(setEmoji())
-}
-function setEmoji(mouseLocation) {
 
-
-}
 
 
 
