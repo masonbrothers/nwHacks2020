d4f0ace276d0474d65e7f5eba3e7075fc1115f08
Jerry Liu <jerryl1996@gmail.com>
2020-01-12 00:44:11 -0800

    Integrated paralleldots with python server

--- server.py
+++ server.py
@@ -1,14 +1,25 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
+from http.server import HTTPServer, SimpleHTTPRequestHandler
 from urllib.parse import urlparse, parse_qs
 
-def get_sentiment(text):
-    print(text)
-    try:
-        return text # b'getting sentement'
-    except:
-        return b'{"error": "cant get sentement"}'
+import paralleldots
+import operator
 
-class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+paralleldots.set_api_key("cJNuFaRhYXRPQPsh19ohGql5hpChB4TBUSt7Y2DRjOk")
+
+def get_emotion(text):
+    emotion_list = paralleldots.emotion(text)['emotion']
+    max_emotion = max(emotion_list.items(), key=operator.itemgetter(1))[0]
+    print(max_emotion)
+    return max_emotion
+
+# def get_sentiment(text):
+#     print(text)
+#     try:
+#         return text # b'getting sentement'
+#     except:
+#         return b'{"error": "cant get sentement"}'
+
+class CORSRequestHandler(SimpleHTTPRequestHandler):
 
     def do_GET(self):
         self.send_response(200)
@@ -23,13 +34,16 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             print(text_array)
             if len(text_array) == 1:
                 text_str = text_array[0]
-                response = get_sentiment(text_str)
+                response = get_emotion(text_str)
                 self.wfile.write(str.encode(response))
             else:
                 self.wfile.write(b'{"error": "text param must be exacly 1 element not an array"}')
         else:
             self.wfile.write(b'{"error": "is text param not specified in get request"}')
+    def end_headers (self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        SimpleHTTPRequestHandler.end_headers(self)
 
 
-httpd = HTTPServer(('localhost', 8000), SimpleHTTPRequestHandler)
+httpd = HTTPServer(('localhost', 8000), CORSRequestHandler)
 httpd.serve_forever()
